520a168b3d859f53a46e6eccff481bab03951628
= <=>
2021-12-21 22:30:15 +0700

    update

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,9 +1,51 @@
 // import models here
+const { product, user } = require('../../models')
 
-exports.getProduct = async (req, res) => {
+exports.getProducts = async (req, res) => {
   // code here
+  try {
+    const data = await product.findAll({
+      include: {
+        model: user,
+        as: 'user',
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'password']
+        }
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt', 'idUser']
+      }
+    })
+
+    res.send({
+      status: 'success...',
+      data
+    })
+  } catch (error) {
+    console.log(error)
+    res.send({
+      status: 'failed',
+      message: 'Server Error'
+    })
+  }
 };
 
 exports.addProduct = async (req, res) => {
   // code here
+  try {
+    const data = req.body
+
+    await product.create(data)
+
+    res.send({
+      status: 'success...',
+      data
+    })
+  } catch (error) {
+    console.log(error)
+    res.send({
+      status: 'failed',
+      message: 'Server Error'
+    })
+  }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,10 @@ const router = express.Router();
 // Controller
 const { addUsers, getUsers, getUser, updateUser, deleteUser } = require("../controllers/user");
 // import controller function here
+const { getProducts, addProduct } = require('../controllers/product')
+const { getTransactions, addTransaction } = require('../controllers/transaction')
+
+
 
 // Route
 router.post("/user", addUsers);
@@ -14,5 +18,10 @@ router.patch("/user/:id", updateUser);
 router.delete("/user/:id", deleteUser);
 
 // add route here
+router.get('/products', getProducts)
+router.post('/product', addProduct)
+
+router.get('/transactions', getTransactions)
+router.post('/transaction', addTransaction)
 
 module.exports = router;
