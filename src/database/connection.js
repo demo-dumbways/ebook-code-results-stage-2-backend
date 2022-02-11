e694a71f78f36234169248ef5c3c8cff2da1c66e
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-11 23:35:21 +0900

    udpate

--- src/database/connection.js
+++ src/database/connection.js
@@ -24,11 +24,18 @@ const sequelize = new Sequelize(connectionString);
 async function checkConn() {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log(
+      '-------------------------------------------------Connection has been established successfully.'
+    );
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error(
+      '-------------------------------------------------Unable to connect to the database:',
+      error
+    );
   }
 }
+
+checkConn();
 
 db.sequelize = sequelize;
 
