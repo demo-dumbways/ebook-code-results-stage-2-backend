6d7717258f770fd5b57ad6bd7881e1c765eb35df
= <=>
2022-02-22 14:06:22 +0700

    Day 2 Week 2 Code Result

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,19 +2,12 @@ const express = require('express')
 
 const router = express.Router()
 
-// Controller
-// import controller function here
 const {
     addUsers,
-    getUsers,
-    getUser
+    getUsers
 } = require('../controllers/user')
 
-
-// Route
 router.post('/user', addUsers)
-// add route here
 router.get('/users', getUsers)
-router.get('/user/:id', getUser)
 
 module.exports = router
